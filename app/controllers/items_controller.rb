efbff9cc13cd969e8bc44f0337f30f30ff780e6f
Abby Anderson <abby.e.anderson@gmail.com>
2021-10-27 18:48:28 -0600

    progress from weds

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,5 +8,29 @@ class ItemsController < Sinatra::Base
         items = Item.all
         items.to_json
     end
+
+    # get a specific item by id
+    get "/items/:id" do
+        item = Item.find(params[:id])
+        item.to_json
+    end
+
+    # to add a new item
+    post "/items" do
+        item = Item.create(
+            name: params[:name],
+            description: params[:description],
+            image_url: params[:image_url],
+            price: params[:price],
+            owner_id: params[:owner_id]
+        )
+        item.to_json
+    end
+
+    # to delete an item by id
+    delete "/items/:id" do
+        item = Item.find(params[:id])
+        item.destroy
+    end
     
 end
